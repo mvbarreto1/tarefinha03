76d11437decb1b670ba50762434f0f8d18ab7463
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-08-16 16:21:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//contador = 1
+//while contador <= 10:
+  //  print(contador);
+    //contador += 1
+
+   // -------
+   //-- questão 2 --
+
+  // int main() {
+    //int numero;
+
+    //do {
+    
+      //  printf("Digite um número: ");
+        //scanf("%d", &numero);
+    //} while (numero >= 0); 
+
+    //printf("Número negativo inserido. Encerrando o programa.\n");
+
+   // return 0;
+//}
+
+//-------
+
+//-- questão 3 ---
+
+//int main() {
+  //  char input[100];
+    //int soma = 0;
+    //int numero;
+    //int valido;
+
+    //printf("Digite números para somar. Insira um valor não numérico para encerrar.\n");
+
+    //while (1) {
+       
+      //  printf("Digite um número: ");
+       // fgets(input, sizeof(input), stdin);
+
+    
+        //valido = sscanf(input, "%d", &numero);
+
+        //if (valido == 1) {
+         
+        //    soma += numero;
+       // } else {
+            
+         //   printf("Valor não numérico detectado. Encerrando...\n");
+           // break;
+        //}
+   // }
+
+    
+   // printf("A soma dos números inseridos é: %d\n", soma);
+
+    //return 0;
+//}
+
+//------------
+
+//-- questao 4 --
